3aa23688c8a41c68e736fd1c6a80bb6578e85772
Kunal <kusdhill@ucsc.edu>
2017-05-12 15:02:53 -0700

    file checking

--- parse.py
+++ parse.py
@@ -1,2 +1,41 @@
 import docx
-print docx.__version__
+import sys
+import os
+
+# verifies that a given filename has .docx extension
+# if it does not, an error is thrown
+def check_extension(filename):
+	extension_flag = 0
+	extension = ""
+	good_extension = "docx"
+
+	for char in filename:
+		if extension_flag==0:
+			if char==".":
+				extension_flag = 1
+		else:
+			extension+=char
+
+	if extension!=good_extension:
+		sys.exit("must pass in .docx files")
+
+def check_existence(filename):
+	if not os.path.isfile(filename):
+		sys.exit("file must exist")
+
+
+
+def main():
+	print("checking command line arguments")
+	if len(sys.argv)!=2:
+		sys.exit("usage: python compare.py filename.docx")
+	else:
+		print("verifying file extension")
+		check_extension(sys.argv[1])
+		print("verifying existence")
+		check_existence(sys.argv[1])
+
+
+
+if __name__ == '__main__':
+	main()
