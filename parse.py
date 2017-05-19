f89cdf6ab6fe782581eb539fe222ecc279703a03
Kunal <kusdhill@ucsc.edu>
2017-05-19 12:55:54 -0700

    guest text separation

--- parse.py
+++ parse.py
@@ -30,25 +30,6 @@ def check_existence(filename):
 
 # parses file for names of guests
 def find_names(doc_object):
-
-	# How to identify a name?
-	# Every name is in bold
-	# Most names are followed by a comma, but not all
-	# Newline (empty paragraph) preceeds every name
-	# Line succeeding every name begins with "Affiliation"
-
-
-	##
-	##for para in doc_object.paragraphs:
-	##	print para.text +"\\n"
-
-	#print("\n"+doc_object.paragraphs[1].runs[1].text)
-
-
-	full_text = ""
-	for para in doc_object.paragraphs:
-		full_text += para.text
-
 	names = []
 	for i in range(0, len(doc_object.paragraphs)):
 		if(len(doc_object.paragraphs[i].runs)>=2):
@@ -57,9 +38,48 @@ def find_names(doc_object):
 				names.append(run.text)
 
 	print(names)
+	return names
+
+# put name and text associated with name into dictionary
+def copy_text(names, doc):
+	name_with_text = {}
+
+	for name in names:
+		name_with_text[name] = "my text"
+
+	print(name_with_text)
+
+
+	# if there are two newlines in a row, start a new index in name_with_text
+
+
+	# everytime you hit a new name, start a new para
+
+	j = 0
+	for i in range(0,len(doc.paragraphs)):
+		line = doc.paragraphs[i]
+		text = line.text
+		bold = False
+
+
+		print(text)
+
+
+		if(len(line.runs)>=2):
+			if(line.runs[1].bold):
+				bold = True
+			else:
+				bold = False
+
+		if names[j] in text and bold:
+			print("found name!")
+			if(j<len(names)-1):
+				j+=1
 
 
 
+
+		
 
 def main():
 	print("checking command line arguments")
@@ -74,7 +94,12 @@ def main():
 	filename = sys.argv[1]
 
 	doc = docx.Document(filename)
-	find_names(doc)
+	print("finding names")
+	names = find_names(doc)
+	print("copying text")
+	copy_text(names, doc)
+	# copy contents between names
+	# open files
 
 
 if __name__ == '__main__':
